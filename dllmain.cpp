d4aadee5ef44050e066c96633657c2ee111532c1
Zack <115175938+ZackWilde27@users.noreply.github.com>
2024-09-06 22:09:56 -0600

    Cleaned up code
    Left-over code has been removed, to make the file cleaner

--- dllmain.cpp
+++ dllmain.cpp
@@ -5,20 +5,16 @@
 
 typedef HRESULT(__stdcall *effectPtr)(LPDIRECT3DDEVICE9, LPCVOID, UINT, const D3DXMACRO*, LPD3DXINCLUDE, DWORD, LPD3DXEFFECTPOOL, LPD3DXEFFECT*, LPD3DXBUFFER*);
 
-typedef HRESULT(__stdcall* createEffectPtr)(LPDIRECT3DDEVICE9, LPCSTR, const D3DXMACRO*, LPD3DXINCLUDE, DWORD, LPD3DXEFFECTPOOL, LPD3DXEFFECT*, LPD3DXBUFFER*);
 effectPtr OriginalCreateEffect = NULL;
 HRESULT hr;
-LPD3DXBUFFER errors;
 
 static HRESULT __stdcall D3DXCreateEffect_Hooked(LPDIRECT3DDEVICE9 pDevice, LPCVOID pSrcData, UINT SrcDataLen, const D3DXMACRO* pDefines, LPD3DXINCLUDE pInclude, DWORD Flags, LPD3DXEFFECTPOOL  pPool, LPD3DXEFFECT* ppEffect, LPD3DXBUFFER* ppCompilationErrors)
 {
     hr = OriginalCreateEffect(pDevice, pSrcData, SrcDataLen, pDefines, pInclude, Flags, pPool, ppEffect, ppCompilationErrors);
     
     if (*ppCompilationErrors)
-    {
         MessageBoxA(NULL, (const char*)(*ppCompilationErrors)->GetBufferPointer(), "Shader Error", MB_ICONERROR);
-        (*ppCompilationErrors)->Release();
-    }
+
     return hr;
 }
 
